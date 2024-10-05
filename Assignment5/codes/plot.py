506f665b4ce46be191eac1faa64bfc411f6959a0
AdisheshBalaji <adishesh.balaji@gmail.com>
2024-10-06 01:09:18 +0530

    revised

--- Assignment5/codes/plot.py
+++ Assignment5/codes/plot.py
@@ -12,7 +12,17 @@ y = points[:, 1]
 
 # Step 5: Plot the ellipse
 plt.figure(figsize=(8, 6))
+a = 4 #semimajor axis
+b = 3 #semiminor axis
+center_x = 0
+center_y = 0
+vertices = [(0,-3),(0,3)]
+covertices = [(4,0),(-4,0)]
+center = (0,0)
 plt.plot(x, y, label='Ellipse', color='blue')
+plt.scatter(*zip(*vertices),color ="red",label="vertices")
+plt.scatter(*zip(*covertices),color = "red", label = "covertices")
+plt.scatter(center[0],center[1],color="red",label = "center")
 plt.fill(x, y, color='lightblue', alpha=0.3)  # Fill the ellipse with color
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
